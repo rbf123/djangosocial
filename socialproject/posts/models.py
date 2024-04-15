1040f227a2356f64af630f43a8fa8b4c0bca5691
Romina <108244092+rbf123@users.noreply.github.com.>
2024-04-15 15:39:17 -0500

    Created a Feed Page and added filter by User, Sorted by most recent

--- socialproject/posts/models.py
+++ socialproject/posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     caption = models.TextField(blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
-    created = models.DateField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
+from .models import Post
+from django.db.models import Q
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
 # Create your views here.
 
 
@@ -16,3 +22,36 @@ def post_create(request):
     else:
         form = PostCreateForm(data=request.GET)
     return render(request,'posts/create.html',{'form': form})
+
+#def feed(request):
+#    posts = Post.objects.all()
+#    return render(request,'posts/feed.html',{'posts':posts})
+
+@login_required
+def feed(request):
+    # Filter by user
+    user_id = request.GET.get('user_id')
+    # Filter by date range
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    posts = Post.objects.all()
+
+    # Most Recent Posts
+    posts = posts.order_by('-created') 
+
+    # Posts by Date
+    if start_date and end_date:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        posts = posts.filter(created__date__range=(start_date, end_date))
+
+    # Posts by User
+    if user_id:
+        posts = posts.filter(user_id=user_id)
+    
+    # Get list of users who have made posts
+    users = User.objects.filter(post__isnull=False).distinct()
+
+    return render(request, 'posts/feed.html', {'posts': posts, 'users':users})
+
